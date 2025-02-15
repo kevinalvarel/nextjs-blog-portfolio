bd652de36ea31961810d18d8c4463528d420c823
kevinalvarel <alvarelkevin12@gmail.com>
2025-02-15 18:06:34 +0700

    edit landing page

--- src/app/(landing-page)/page.js
+++ src/app/(landing-page)/page.js
@@ -1,7 +1,6 @@
 import ProjectSection from "./components/ProjectSection";
 import PostsSection from "./components/PostsSection";
 import HeroSection from "./components/HeroSection";
-import About from "./components/About";
 
 export default function Home() {
   return (
