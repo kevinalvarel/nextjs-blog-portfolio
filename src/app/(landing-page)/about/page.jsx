7f303d45f7432897520d167b031f2314d544be57
kevinalvarel <alvarelkevin12@gmail.com>
2025-04-17 21:02:58 +0700

    fix bug

--- src/app/(landing-page)/about/page.jsx
+++ src/app/(landing-page)/about/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import ScrollFloat from "./components/ScrollFloat";
-import BlurText from "./components/BlurText";
+
 import SplashCursor from "./components/SplashCursor";
 
 const Page = () => {
@@ -9,13 +9,16 @@ const Page = () => {
     <div className="container">
       <div className="mx-10 justify-items-center items-center py-5">
         <div className="py-36 md:py-40 sm:py-44 mb-28">
-          <BlurText
-            text="Hello"
-            delay={150}
-            animateBy="letters"
-            direction="bottom"
-            className="font-extrabold text-5xl"
-          />
+          <ScrollFloat
+            animationDuration={1}
+            ease="back.inOut(2)"
+            scrollStart="center bottom+=80%"
+            scrollEnd="bottom bottom-=40%"
+            stagger={0.3}
+            textClassName="font-bold"
+          >
+            Hello
+          </ScrollFloat>
           <SplashCursor />
         </div>
         <div className="py-36 md:py-40 sm:py-44">
