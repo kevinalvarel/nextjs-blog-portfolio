849949cdb35a46ff820db79e12c5750b5ea7ed20
kevinalvarel <alvarelkevin12@gmail.com>
2025-04-24 19:43:01 +0700

    fixing bug 2

--- payload.config.js
+++ payload.config.js
@@ -5,12 +5,11 @@ import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
 import { Posts } from "./src/app/collections/Posts";
 import { Media } from "@/app/collections/Media";
-import { Chapters } from "@/app/collections/Chapter";
 export default buildConfig({
   // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor(),
   // Define and configure your collections in this array
-  collections: [Posts, Media, Chapters],
+  collections: [Posts, Media],
   plugins: [
     vercelBlobStorage({
       enabled: true, // Optional, defaults to true
