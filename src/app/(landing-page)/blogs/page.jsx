410110c92566011a2e3aa5ac00efe8f2e20c5dc5
kevinalvarel <alvarelkevin12@gmail.com>
2025-07-15 19:02:37 +0700

    dynamic-page

--- src/app/(landing-page)/blogs/page.jsx
+++ src/app/(landing-page)/blogs/page.jsx
@@ -2,6 +2,8 @@ import BlogList from "./components/BlogList";
 import Header from "./components/Header";
 import LatestBlog from "./components/LatestBlog";
 
+export const dynamic = "force-dynamic";
+
 const Posts = async () => {
   return (
     <div className="container mx-auto p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
